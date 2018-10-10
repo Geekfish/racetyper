d671b2eeb37a4f8fe433a4ffc8d02a81643fd3de
Coen de Groot <cdegroot@nomensa.com>
2018-10-10 20:21:02 +0100

    Jokes cleaned up

--- moving_cars.py
+++ moving_cars.py
@@ -4,6 +4,20 @@ import random
 
 from text import AQUA, BANANA, WHITE, text_to_screen, TEXT
 
+
+class Lines:
+    def __init__(self):
+        with open('jokes.txt') as input_file:
+            self.lines = [l.strip() for l in input_file.readlines() if l.strip()]
+        random.shuffle(self.lines)
+        self.line_number = -1
+
+    @property
+    def next(self):
+        self.line_number += 1
+        return self.lines[self.line_number]
+
+
 pygame.init()
 
 size = width, height = 1024, 768
@@ -20,7 +34,10 @@ car1_x = width - 150
 car2_y = 650
 car2_x = width - 150
 
-text = TEXT
+lines = Lines()
+text = lines.next
+
+
 
 while 1:
     for event in pygame.event.get():
@@ -32,6 +49,8 @@ while 1:
 
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_SPACE]:
+        if len(text) < 20:
+            text += '     ..      ' + lines.next
         text = text[1:]
 
 
